fff7c49de65e649c65cae98d3f58616d683c9665
LucSosa <l.sosamachado@gmail.com>
2022-04-06 13:51:41 -0300

    Streams commit 01

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/ZZEstreams/domain/LightNovel.java
@@ -0,0 +1,42 @@
+package academy.devdojo.maratonajava.javacore.ZZEstreams.domain;
+
+import java.util.Objects;
+
+public class LightNovel {
+    private String name;
+    private double price;
+
+    public LightNovel(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LightNovel that = (LightNovel) o;
+        return Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        return "LightNovel{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
