9addc186c079d61db547452dc690e8dd271a06a3
LucSosa <l.sosamachado@gmail.com>
2022-04-13 08:08:07 -0300

    Concorrencia commit final

--- maratona-java/src/academy/devdojo/maratonajava/javacore/ZZGconcorrencia/test/FutureTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/ZZGconcorrencia/test/FutureTest01.java
@@ -3,14 +3,24 @@ package academy.devdojo.maratonajava.javacore.ZZGconcorrencia.test;
 import java.util.concurrent.*;
 
 public class FutureTest01 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException, TimeoutException {
         ExecutorService executorService = Executors.newSingleThreadExecutor();
         Future<Double> dollar = executorService.submit(() -> {
-            TimeUnit.SECONDS.sleep(2);
+
+            TimeUnit.SECONDS.sleep(15);
             return 4.35D;
         });
+        System.out.println(doingSomething());
+        Double dollarResponse = dollar.get(3,TimeUnit.SECONDS);
+        System.out.println("Dollar" + dollarResponse);
+        executorService.shutdown();
     }
-    private static void doingSomething(){
-
+    private static long doingSomething(){
+        System.out.println(Thread.currentThread().getName());
+        long sum = 0;
+        for (int i = 0; i < 1_000_000; i++) {
+            sum +=i ;
+        }
+        return sum;
     }
 }
