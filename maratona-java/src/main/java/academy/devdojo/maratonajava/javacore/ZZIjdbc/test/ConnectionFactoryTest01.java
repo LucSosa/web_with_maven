a9488a27acda70425727ffe471b032b36b9a1061
LucSosa <l.sosamachado@gmail.com>
2022-04-20 13:38:18 -0300

    jdbc commit 03

--- maratona-java/src/main/java/academy/devdojo/maratonajava/javacore/ZZIjdbc/test/ConnectionFactoryTest01.java
+++ maratona-java/src/main/java/academy/devdojo/maratonajava/javacore/ZZIjdbc/test/ConnectionFactoryTest01.java
@@ -3,17 +3,27 @@ package academy.devdojo.maratonajava.javacore.ZZIjdbc.test;
 import academy.devdojo.maratonajava.javacore.ZZIjdbc.conn.ConnectionFactory;
 import academy.devdojo.maratonajava.javacore.ZZIjdbc.domain.Producer;
 import academy.devdojo.maratonajava.javacore.ZZIjdbc.repository.ProducerRepository;
+import academy.devdojo.maratonajava.javacore.ZZIjdbc.service.ProducerService;
 import lombok.extern.log4j.Log4j2;
+
+import java.util.List;
 
 @Log4j2
 public class ConnectionFactoryTest01 {
     public static void main(String[] args) {
-    //    Producer producer = Producer.builder().name("Studio Deen").build();
-    //    ProducerRepository.save(producer);
-       log.info("info");
-       log.info("debug");
-       log.info("warn");
-       log.info("error");
-       log.info("trace");
+        Producer producer = Producer.builder().name("Studio Deen").build();
+        Producer producerToUpdate = Producer.builder().id(1).name("MADHOUUSE").build();
+//        ProducerService.save(producer);
+//        ProducerService.delete(4);
+//        ProducerService.delete(5);
+//        ProducerService.update(producerToUpdate);
+//        List<Producer> producers = ProducerService.findAll();
+        List<Producer> producers = ProducerService.findByName("Mad");
+        log.info("Producers found '{}'", producers);
+//       log.info("info");
+//       log.info("debug");
+//       log.info("warn");
+//       log.info("error");
+//       log.info("trace");
     }
 }
