4af9cd02fae74ab9b44dc6c213d1d0c2c58c0187
LucSosa <l.sosamachado@gmail.com>
2022-04-25 21:52:40 -0300

    jdbc commit 07

---
+++ maratona-java/src/main/java/academy/devdojo/maratonajava/javacore/ZZIjdbc/service/ProducerServiceRowSet.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.javacore.ZZIjdbc.service;
+
+import academy.devdojo.maratonajava.javacore.ZZIjdbc.domain.Producer;
+import academy.devdojo.maratonajava.javacore.ZZIjdbc.repository.ProducerRepositoryRowSet;
+
+import java.util.List;
+
+public class ProducerServiceRowSet {
+
+    public static List<Producer> findByNameJdbcRowSet(String name){
+        return ProducerRepositoryRowSet.findByNameJdbcRowSet(name);
+    }
+
+    public static void updateJdbcRowSet(Producer producer){
+        ProducerRepositoryRowSet.updateJdbcRowSet(producer);
+    }
+
+
+
+}
